df1f9a8d85f62976d4f038ab547a9d4b51011b1c
amwi5704 <77754630+amwi5704@users.noreply.github.com>
2022-05-02 17:53:22 -0600

    fhgh

--- FinalProject.js
+++ FinalProject.js
@@ -1,6 +1,9 @@
-const customName = document.getElementById('customname');
-const normalButton = document.querySelector('.normalButton');
-const story = document.querySelector('.story');
+const button = document.querySelector('.generate');
+const sand = document.querySelector('.mainsS');
+const gob = document.querySelector('.mainsG');
+const ha = document.querySelector('.mainsH');
+const atype = document.querySelector('.atype');
+const level = document.querySelector('.level');
 
 function randomValueFromArray(array){
   const random = Math.floor(Math.random()*array.length);
@@ -9,43 +12,254 @@ function randomValueFromArray(array){
 
 console.log('loaded');
 
-const storyText = "It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.";
-const insertX = ["Willy the Goblin", "Big Daddy", "Father Christmas"];
-const insertY = ["the soup kitchen", "Disneyland", "the White House"];
-const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewalk", "turned into a slug and crawled away"];
+const percentages = ["ATK %", "DEF %", "Energy Recharge", "Crit Rate", "Crit Damage"];
+const allStats = ["ATK %", "DEF %", "Energy Recharge", "Crit Rate", "Crit Damage", "ATK", "HP", "DEF", "Elemental Mastery"];
 const critR = [2.7, 3.1, 3.5, 3.9];
 const critD = [5.4, 6.2, 7.0, 7.8];
+const defF = [16, 19, 21, 23];
+const defP = [5.1, 6.2, 7.3];
+const atkF = [14, 17, 19];
+const atkP = [4.1, 4.6, 5.2, 5.8];
+const hpF = [209, 229, 269, 299];
+const hpP = [4.1, 4.6, 5.2, 5.8];
+const em = [16, 19, 21, 23];
+const er = [4.5, 5.5, 6.5];
+
+atype.addEventListener('click', check);
+
+function check(){
+if(document.getElementById("feather").checked) {
+    console.log('plume');
+    ha.style.visibility = 'hidden';
+    gob.style.visibility = 'hidden';
+    sand.style.visibility = 'hidden';
+}
+else if(document.getElementById("flower").checked) {
+    console.log('broach');
+    ha.style.visibility = 'hidden';
+    gob.style.visibility = 'hidden';
+    sand.style.visibility = 'hidden';
+}
+else if(document.getElementById("sands").checked) {
+    sand.style.visibility = 'visible';
+    console.log('timepiece');
+    ha.style.visibility = 'hidden';
+    gob.style.visibility = 'hidden';
+}
+else if(document.getElementById("goblet").checked) {
+    gob.style.visibility = 'visible';
+    console.log('cup');
+    sand.style.visibility = 'hidden';
+    ha.style.visibility = 'hidden';
+}
+else if(document.getElementById("hat").checked) {
+    ha.style.visibility = 'visible';
+    console.log('circlit');
+    sand.style.visibility = 'hidden';
+    gob.style.visibility = 'hidden';
+}}
+
+let artifact = {
+  "mainStat": "DEF %",
+  "mainStatV": 0,
+  "sub1": "DEF",
+  "sub1v": 0,
+  "p1": false,
+  "sub2": "HP",
+  "sub2v": 0,
+  "p2": false,
+  "sub3": "HP %",
+  "sub3v": 0,
+  "p3": true,
+  "sub4": "ATK",
+  "sub4v": 0,
+  "p4": false,
+  "lv": 0
+};
+
+button.addEventListener('click', generate);
+
+function generate(){
+let mainStat = "";
+let sub1p = false;
+let sub2p = false;
+let sub3p = false;
+let sub4p = false;
+
+if (document.getElementById("feather").checked){
+  mainStat = "ATK";
+}
+else if (document.getElementById("flower").checked){
+  mainStat = "HP";
+}
+else if (document.getElementById("sands").checked){
+  mainStat = document.querySelector('input[name="mainSS"]:checked').value;
+;
+}
+else if (document.getElementById("goblet").checked){
+  mainStat = document.querySelector('input[name="mainSG"]:checked').value;
+}
+else if (document.getElementById("hat").checked){
+  mainStat = document.querySelector('input[name="mainSH"]:checked').value;
+};
+
+if (percentages.includes(document.getElementById("cs1").value)){
+  sub1p = true;
+}
+if (percentages.includes(document.getElementById("cs2").value)){
+  sub2p = true;
+}
+if (percentages.includes(document.getElementById("cs3").value)){
+  sub3p = true;
+}
+if (percentages.includes(document.getElementById("cs4").value)){
+  sub4p = true;
+}
 
 
-normalButton.addEventListener('click', result);
+artifact = {
+  "mainStat": mainStat,
+  "mainStatV": 1,
+  "sub1": document.getElementById("cs1").value,
+  "sub1v": parseFloat(document.getElementById("cs1v").value),
+  "p1": sub1p,
+  "sub2": document.getElementById("cs2").value,
+  "sub2v": parseFloat(document.getElementById("cs2v").value),
+  "p2": sub2p,
+  "sub3": document.getElementById("cs3").value,
+  "sub3v": parseFloat(document.getElementById("cs3v").value),
+  "p3": sub3p,
+  "sub4": document.getElementById("cs4").value,
+  "sub4v": parseFloat(document.getElementById("cs4v").value),
+  "p4": sub4p,
+  "lv": 0
+};
+console.log(artifact);
+}
 
-function result() {
-   newStory = storyText;
-    const xItem = randomValueFromArray(insertX);
-    const yItem = randomValueFromArray(insertY);
-    const zItem = randomValueFromArray(insertZ);
+level.addEventListener('click', levelup);
 
-    newStory = newStory.replace(":insertx:", xItem);
-    newStory = newStory.replace(":insertx:", xItem);
-    newStory = newStory.replace(":inserty:", yItem);
-    newStory = newStory.replace(":insertz:", zItem);
+function levelup(){
+  if(artifact.sub4 === "none"){
+    addFourthStat();
+    console.log(artifact);
+  }
+  else if(artifact.lv === 20){
+    console.log("maxxed");
+  }
+  else{
+    plusFour();
+  };
+}
 
-
-
-  if(customName.value !== '') {
-    const name = customName.value;
-
-    newStory = newStory.replace("Bob", name);
+function addFourthStat(){
+  console.log("yay fourth stat");
+  let arr = [artifact.sub1, artifact.sub2, artifact.sub3, artifact.sub4, artifact.mainStat];
+  let newS = randomValueFromArray(allStats);
+  if (arr.includes(newS))
+  {
+    addFourthStat();
+  }
+  else{
+    artifact.sub4 = newS;
+    if (newS === "ATK %"){
+      artifact.sub4v = randomValueFromArray(atkP);
+    }
+    else if (newS === "ATK"){
+      artifact.sub4v = randomValueFromArray(atkF);
+    }
+    else if (newS === "DEF %"){
+    artifact.sub4v = randomValueFromArray(defP);
+    }
+    else if (newS === "DEF"){
+      artifact.sub4v = randomValueFromArray(defF);
+    }
+    else if (newS === "HP %"){
+      artifact.sub4v = randomValueFromArray(hpP);
+    }
+    else if (newS === "HP"){
+      artifact.sub4v = randomValueFromArray(hpF);
+    }
+    else if (newS === "Energy Recharge"){
+      artifact.sub4v = randomValueFromArray(er);
+    }
+    else if (newS === "Elemental Mastery"){
+      artifact.sub4v = randomValueFromArray(em);
+    }
+    else if (newS === "Crit Rate"){
+      artifact.sub4v = randomValueFromArray(critR);
+    }
+    else if (newS === "Crit Damage"){
+      artifact.sub4v = randomValueFromArray(critD);
+    };
   }
 
-  if(document.getElementById("uk").checked) {
-    const weight = Math.round(300) + ' stone';
-    const temperature =  Math.round(94) + ' centigrade';
 
-    newStory = newStory.replace('94 fahrenheit', temperature);
-    newStory = newStory.replace('300 pounds', weight);
+}
+
+function plusFour(){
+  console.log("plus 4");
+  artifact.lv = artifact.lv +4;
+  let arr = [artifact.sub1, artifact.sub2, artifact.sub3, artifact.sub4];
+  const rand = Math.floor(Math.random()*4) +1;
+  let sub = "";
+  if (rand === 1){
+    sub = artifact.sub1;
+    subv = artifact.sub1v;
   }
-
-  story.textContent = newStory;
-  story.style.visibility = 'visible';
+  else if (rand === 2){
+    sub = artifact.sub2;
+    subv = artifact.sub2v;
+  }
+  else if (rand === 3){
+    sub = artifact.sub3;
+    subv = artifact.sub3v;
+  }
+  else if (rand === 4){
+    sub = artifact.sub4;
+    subv = artifact.sub4v;
+  };
+  if (sub === "ATK %"){
+    subv = subv + randomValueFromArray(atkP);
+  }
+  else if (sub === "ATK"){
+    subv = subv + randomValueFromArray(atkF);
+  }
+  else if (sub === "DEF %"){
+  subv = subv + randomValueFromArray(defP);
+  }
+  else if (sub === "DEF"){
+    subv = subv + randomValueFromArray(defF);
+  }
+  else if (sub === "HP %"){
+    subv = subv + randomValueFromArray(hpP);
+  }
+  else if (sub === "HP"){
+    subv = subv + randomValueFromArray(hpF);
+  }
+  else if (sub === "Energy Recharge"){
+    subv = subv + randomValueFromArray(er);
+  }
+  else if (sub === "Elemental Mastery"){
+    subv = subv + randomValueFromArray(em);
+  }
+  else if (sub === "Crit Rate"){
+    subv = subv + randomValueFromArray(critR);
+  }
+  else if (sub === "Crit Damage"){
+    subv = subv + randomValueFromArray(critD);
+  };
+  if (rand === 1){
+    artifact.sub1v = subv;
+  }
+  else if (rand === 2){
+    artifact.sub2v = subv;
+  }
+  else if (rand === 3){
+    artifact.sub3v = subv;;
+  }
+  else if (rand === 4){
+    artifact.sub4v = subv;;
+  };
+  console.log(artifact);
 }
